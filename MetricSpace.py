53eecf679af34d97b142b03309086846560d859c
emmanuel-hartman <76754839+emmanuel-hartman@users.noreply.github.com>
2022-12-09 16:33:00 -0500

    InitCommit

---
+++ MetricSpace.py
@@ -0,0 +1,73 @@
+"""
+"""
+import abc
+import torch
+device = torch.device("cuda:0")
+
+class MetricSpace(abc.ABC):
+    r"""Class for metric space.
+    Parameters
+    ----------
+    dim : int
+        Dimension of the metric space.
+    shape : tuple of int
+        Shape of one element of the metric space.
+        Optional, default : None.
+    Attributes
+    ----------
+    distance : 
+        distance on the metric space.
+    point_ndim : int
+        Dimension of point array.
+    """
+
+    def __init__(self, dim, shape, **kwargs):
+        super().__init__(**kwargs)
+
+        self.dim = dim
+        self.shape = shape
+        self.point_ndim = len(self.shape)
+
+    @abc.abstractmethod
+    def belongs(self, point, atol):
+        """Evaluate if a point belongs to the metric space.
+        Parameters
+        ----------
+        point : array-like, shape=[..., *point_shape]
+            Point to evaluate.
+        atol : float
+            Absolute tolerance.
+        Returns
+        -------
+        belongs : array-like, shape=[...,]
+            Boolean evaluating if point belongs to the metric space.
+        """
+        
+    @abc.abstractmethod
+    def random_point(self, n_samples=1):
+        """Sample random points on the metric space according to some distribution.
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples.
+            Optional, default: 1.
+        Returns
+        -------
+        samples : array-like, shape=[..., *point_shape]
+            Points sampled in the metric space.
+        """
+
+    @abc.abstractmethod
+    def distance(self,point1,point2):
+        """Compute the distance between two points.
+        Parameters
+        ----------
+        point1 : array-like, shape=[..., *point_shape]
+            Point to evaluate.
+        point2 : array-like, shape=[..., *point_shape]
+            Point to evaluate.
+        Returns
+        -------
+        belongs : array-like, shape=[..., ..., 1]
+            Float evaluating the distance between two points in the metric space.
+        """
---
+++ Sphere.py
@@ -0,0 +1,59 @@
+import torch
+device = torch.device("cuda:0")
+
+
+class Sphere(MetricSpace):
+    """
+    """
+    
+    def __init__(self, dim):
+        self.dim = dim
+        shape=dim+1
+        super().__init__(dim=dim)
+      
+    def belongs(self, point, atol):
+        """Evaluate if a point belongs to the metric space.
+        Parameters
+        ----------
+        point : array-like, shape=[..., *point_shape]
+            Point to evaluate.
+        atol : float
+            Absolute tolerance.
+        Returns
+        -------
+        belongs : array-like, shape=[...,]
+            Boolean evaluating if point belongs to the metric space.
+        """
+        
+        return torch.isclose(torch.linalg.norm(point,dim=0), torch.ones((point.shape[0])), atol=atol)
+        
+        
+    def random_point(self, n_samples=1):
+        """Sample random points on the metric space according to some distribution.
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples.
+            Optional, default: 1.
+        Returns
+        -------
+        samples : array-like, shape=[..., *point_shape]
+            Points sampled in the metric space.
+        """
+        return 
+    
+    def distance(self,point1,point2):
+        """Compute the distance between two points.
+        Parameters
+        ----------
+        point1 : array-like, shape=[..., *point_shape]
+            Point to evaluate.
+        point2 : array-like, shape=[..., *point_shape]
+            Point to evaluate.
+        Returns
+        -------
+        belongs : array-like, shape=[..., ..., 1]
+            Float evaluating the distance between two points in the metric space.
+        """
+        
+        return 
