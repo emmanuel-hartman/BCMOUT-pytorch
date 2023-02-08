Implemented algorithm, first attempt. Had to modify random method for Measures due to tensor size mismatch. Need to fix calculation of Omega. Signed-off-by: dylanmarcusconnell <dylmarcon@gmail.com>

MeasuresSpace.py
@@ -28,8 +28,8 @@ class MeasuresSpace:
         ls=[]
         for i in range(0,n_samples):
             supports = self.M.random(n_supports)
-            masses = maxWeight*torch.rand(1,n_supports)
-            point = torch.concat([masses,supports], dim=0)
+            masses = maxWeight*torch.rand(n_supports,1)
+            point = torch.concat([masses,supports], dim=1)
             ls+=[point]
         
         return ls
@@ -43,26 +43,71 @@ class MeasuresSpace:
             belongs[i]=torch.all(self.CoM.belongs(point))
         return belongs
     
-    def _pairwise_distance(self,point1,point2):
-        point1=point1.to(device)
-        point2=point2.to(device)
-        a = point1[0,:]
-        b = point2[0,:]
-        u = point1[1:,:]
-        v = point2[1:,:]
-        Omega1 = self.CoM._energy(u,v)
+    def _pairwise_distance(self,point1,point2, steps, eps):
+        a = point1[0,:].to(device) #measures of point1
+        b = point2[0,:].to(device) #measures of point2
+        u = point1[1:,:].to(device) #supports of point1
+        v = point2[1:,:].to(device) #supports of point2
+        m = a.size(dim=0) #number of supports of point 1
+        n = b.size(dim=0) #number of supports of point 2
+        Omega1 = self.CoM._energy(u,v).to(device)
         Omega = torch.zeros((Omega1.shape[0]+1,Omega1.shape[1]+1)).to(device)
         Omega[1:,1:]= Omega1
-        colsum = torch.sum(Omega1, 1).to(device)
-        rowsum = torch.sum(Omega1, 0).to(device)
-        Omega[0,:] = colsum
-        Omega[:,0] = rowsum
+        rowsum = torch.sum(Omega,0)
+        colsum = torch.sum(Omega,1)
+        Omega[0,:] = torch.div(rowsum,torch.linalg.norm(rowsum, dim=0))
+        Omega[:,0] = torch.div(colsum,torch.linalg.norm(colsum, dim=0))
+
+        cost = torch.zeros([steps])
+
+        P = Omega
+        P[P<0] = 0
+        Q = P #Initialize a semi-coupling
+
+        for k in range(0,steps):
+            P,Q = _contractionRowCol(P,Q,Omega,a,b,m,n)
+            cost[k+1,:]=_cost(P,Q,Omega,a,b,m,n)
+            ind = k+1
+            if (cost[k+1]-cost[k,:])/cost[k+1] < eps:
+                break
+        dist = torch.sqrt(sum(a.cpu())+sum(b.cpu())-2*calcF(P,Q,Omega).cpu())
+        return dist,cost,ind,P,Q
+            
+        
+        
 
     def distance(self, point1, point2):
+        
         distance = torch.zeros((len(point1),len(point2)))
         for i in range(0, distance.shape[0]):
             for j in range(0,distance.shape[1]):
                 distance[i,j]=self._pairwise_distance(point1[i], point2[j])
         return distance
-
     
+    def _cost(self, P, Q, Omega):
+        c = torch.sum(P*Q*Omega)
+        return c
+
+    def _rowNormalize(P_new, a, n):
+        RowNormP_new = torch.sqrt(torch.sum(P_new*P_new,dim=1)/a.transpose(0,1))
+        RowNormMatrix = RowNormP_new.repeat([n,1]).transpose(0,1)
+        P_newNormalized = P_new/RowNormMatrix
+        return P_newNormalized
+    def _colNormalize(Q_new, b, m):
+        ColNormQ_new = torch.sqrt(torch.sum(Q_new*Q_new,dim=0)/b.transpose(0,1))
+        ColNormMatrix = ColNormQ_new.repeat([m,1])
+        Q_newNormalized = Q_new/RowNormalMatrix
+        return Q_newNormalized
+
+    def _contractionRowCol(P,Q,Omega,a,b,m,n):
+        P = _rowNormalize(Q*Omega,a,n)
+        Q = _colNormalize(P*Omega,b,m)
+        return P,Q
+    
+    def _getAssignment(a,b,m,n,Omega,steps,eps):
+        P = Omega
+        P[P<0] = 0
+        Q = P
+        for j in range(0,steps):
+            P,Q = _contractionRowCol(P,Q,Omega,a,b,m,n)
+        return P,Q

Sphere.py
@@ -10,7 +10,7 @@ class Sphere(MetricSpace):
     
     def __init__(self, dim):
         self.dim = dim
-        self.shape= dim+1
+        self.shape= [dim + 1]
         super().__init__()
       
     def belongs(self, points, atol=1e-6):
@@ -47,7 +47,7 @@ class Sphere(MetricSpace):
             Points sampled in the metric space.
         """
         
-        points = 2*torch.rand(self.shape,samples)-1
+        points = torch.rand([samples] + self.shape)
         return points/torch.linalg.norm(points,dim=0)
     
     def distance(self,point1,point2):

Test.py
@@ -1,6 +1,28 @@
-from Sphere import Hypersphere
+import torch
+import numpy as np
+from MeasuresSpace import MeasuresSpace
+from Sphere import Sphere
+device = torch.device("cuda:0")
+M = Sphere(3)
+Measures = MeasuresSpace(M, 2)
+k = Measures.random(1,3,4)
+print(k)
+#Omega1 = Measures.CoM._energy(u,v)
 
-a = Hypersphere(2,5)
-b = a.random(5)
-print(a)
-print(b)
+#R = torch.eye(3).to(device)
+#Omega2 = torch.einsum('kl,ik,jl->ij',R,u,v)
+
+#print(Omega1)
+#print(Omega2)
+
+#Omega = torch.zeros((Omega1.shape[0]+1,Omega1.shape[1]+1)).to(device)
+#Omega[1:,1:]= Omega1
+#rowsum = torch.sum(Omega,0)
+#colsum = torch.sum(Omega,1)
+#Omega[0,:] = torch.div(rowsum,rowsum)
+#Omega[:,0] = torch.div(colsum,colsum)
+#Omega[0,0] = 0
+#P = Omega
+#P[P<0] = 0
+#Q = P
+