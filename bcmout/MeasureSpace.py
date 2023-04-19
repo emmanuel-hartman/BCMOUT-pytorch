1d0e4c580558aa59a9de3e658c31ff0dc8164fb1
emmanuel-hartman <76754839+emmanuel-hartman@users.noreply.github.com>
2023-04-19 03:39:19 -0400

    BCM decoupling we discussed today

--- bcmout/MeasureSpace.py
+++ bcmout/MeasureSpace.py
@@ -3,6 +3,7 @@ import torch
 from bcmout.MetricSpace import MetricSpace 
 from bcmout.Metric import Metric
 from bcmout.ConeSpace import ConeOverM
+from bcmout.BCMSolver import BCMSolver
 
 class MeasureSpace(MetricSpace):
     """ Class for the metric space defined by the cone over a metric space
@@ -24,7 +25,7 @@ class MeasureSpace(MetricSpace):
             kwargs.setdefault("metric", WFRMetric(M,delta,use_cuda))
         elif metric=="GH":
             self.CoM = ConeOverM(M,delta,metric="Exp")
-            kwargs.setdefault("metric", CosBarMetric(M,delta,use_cuda))
+            kwargs.setdefault("metric", GHMetric(M,delta,use_cuda))
         else:
             self.CoM = ConeOverM(M,delta,metric="CosBar")
             kwargs.setdefault("metric", WFRMetric(M,delta,use_cuda))
@@ -96,7 +97,7 @@ class WFRMetric(Metric):
         self.M=M
         self.delta =delta
         self.CoM = ConeOverM(M,delta)
-        self.solver=BCDSolver(self.CoM, use_cuda)
+        self.solver=BCMSolver(use_cuda)
         super().__init__()
 
     def distance(self,point1,point2=None,max_steps=10000,eps=1e-5):  
@@ -117,14 +118,25 @@ class WFRMetric(Metric):
             for i in range(0, distance.shape[0]):
                 for j in range(0,distance.shape[1]):
                     print(str(i*distance.shape[0]+j+1)+"/"+str(distance.shape[0]*distance.shape[1]), end="\r")
-                    distance[i,j]=self.solver._pairwise_distance(point1[i], point2[j],max_steps,eps)
-                    distance[j,i]=distance[i,j]
+                    a = point1[i][0,:]
+                    b = point2[j][0,:]
+                    u = point1[i][1:,:]
+                    v = point2[j][1:,:]
+                    Omega = self.CoM._energy(u,v)
+                    P,Q = self.solver.getOptimalPQ(a,b,Omega,max_steps,eps)
+                    distance[i,j]=self.solver.getDistFromPQ(P,Q,a,b,Omega,self.CoM.delta)
         else:
             distance = torch.zeros((len(point1),len(point1)))
             for i in range(0, distance.shape[0]):
                 for j in range(i+1,distance.shape[1]):
                     print(str(i*distance.shape[0]+j+1)+"/"+str(distance.shape[0]*distance.shape[1]-distance.shape[0]), end="\r")
-                    distance[i,j]=self.solver._pairwise_distance(point1[i], point1[j],max_steps,eps)
+                    a = point1[i][0,:]
+                    b = point1[j][0,:]
+                    u = point1[i][1:,:]
+                    v = point1[j][1:,:]
+                    Omega = self.CoM._energy(u,v)
+                    P,Q = self.solver.getOptimalPQ(a,b,Omega,max_steps,eps)
+                    distance[i,j]=self.solver.getDistFromPQ(P,Q,a,b,Omega,self.CoM.delta)
                     distance[j,i]=distance[i,j]
         return distance
     
@@ -144,7 +156,7 @@ class GHMetric(Metric):
         self.M=M
         self.delta =delta
         self.CoM = ConeOverM(M,delta,metric="Exp")
-        self.solver=BCDSolver(self.CoM, use_cuda)
+        self.solver=BCMSolver(use_cuda)
         super().__init__()
 
     def distance(self,point1,point2=None,max_steps=10000,eps=1e-5):  
@@ -165,84 +177,22 @@ class GHMetric(Metric):
             for i in range(0, distance.shape[0]):
                 for j in range(0,distance.shape[1]):
                     print(str(i*distance.shape[0]+j+1)+"/"+str(distance.shape[0]*distance.shape[1]), end="\r")
-                    distance[i,j]=self.solver._pairwise_distance(point1[i], point2[j],max_steps,eps)
-                    distance[j,i]=distance[i,j]
+                    a = point1[i][0,:]
+                    b = point2[j][0,:]
+                    u = point1[i][1:,:]
+                    v = point2[j][1:,:]
+                    Omega = self.CoM._energy(u,v)
+                    P,Q = self.solver.getOptimalPQ(a,b,Omega,max_steps,eps)
         else:
             distance = torch.zeros((len(point1),len(point1)))
             for i in range(0, distance.shape[0]):
                 for j in range(i+1,distance.shape[1]):
                     print(str(i*distance.shape[0]+j+1)+"/"+str(distance.shape[0]*distance.shape[1]-distance.shape[0]), end="\r")
-                    distance[i,j]=self.solver._pairwise_distance(point1[i], point1[j],max_steps,eps)
+                    a = point1[i][0,:]
+                    b = point1[j][0,:]
+                    u = point1[i][1:,:]
+                    v = point1[j][1:,:]
+                    Omega = self.CoM._energy(u,v)
+                    P,Q = self.solver.getOptimalPQ(a,b,Omega,max_steps,eps)
                     distance[j,i]=distance[i,j]
         return distance
-    
-    
-
-class BCDSolver: 
-    """Class for a BCD solver for Kantorovich formulations of OUT.
-    
-    Parameters
-    ----------
-    CoM : Metric Space Object
-        cone space object used for the initialization of the BCD solver
-    use_cuda : bool 
-        parameter that defines whether to use cuda.
-    """ 
-    def __init__(self,CoM, use_cuda):
-        self.CoM = CoM
-        self.device = torch.device("cuda:0" if use_cuda else "cpu")
-        
-    def _contractionRowCol(self,P,Q,Omega,a,b,m,n):
-        P = self._rowNormalize(Q*Omega,a,n)
-        Q = self._colNormalize(P*Omega,b,m)
-        return P,Q  
-    
-    def _rowNormalize(self,Pnew,a,n):
-        sums = torch.sum(Pnew*Pnew,dim=0)
-        zeros = sums==0
-        RowNormPnew = torch.sqrt(sums.reshape(1,-1)/a.reshape(1,-1))
-        RowNormMatrix = RowNormPnew.repeat([n,1])
-        
-        Pnew[:,zeros]=0
-        RowNormMatrix[:,zeros]=1
-        PnewNormalized = Pnew/RowNormMatrix
-        return PnewNormalized
-    
-    def _colNormalize(self,Qnew,b,m):
-        sums = torch.sum(Qnew*Qnew,dim=1)
-        zeros = sums==0
-        ColumnNormQnew = torch.sqrt(sums.reshape(1,-1)/b.reshape(1,-1))
-        ColumnNormMatrix = ColumnNormQnew.repeat([m,1]).transpose(0,1)
-        Qnew[zeros,:]=0
-        ColumnNormMatrix[zeros,:]=1
-        QnewNormalized = Qnew/ColumnNormMatrix
-        return QnewNormalized
-    
-    def _calcF(self,P,Q,Omega):
-        cost=torch.sum(P*Q*Omega)
-        return cost
-
-    
-    def _pairwise_distance(self,point1,point2,max_steps,eps):
-        point1=point1.to(self.device)
-        point2=point2.to(self.device)
-        a = point1[0,:]
-        b = point2[0,:]
-        m = a.shape[0]
-        n = b.shape[0]
-        u = point1[1:,:]
-        v = point2[1:,:]
-        Omega = self.CoM._energy(u,v)
-        P = Omega
-        Q = Omega
-        
-        cost=torch.zeros((max_steps+1,1))
-        for k in range(0,max_steps):
-            P,Q=self._contractionRowCol(P,Q,Omega,a,b,m,n)
-            cost[k+1,:]=self._calcF(P,Q,Omega).cpu()
-            ind=k+1
-            if (cost[k+1]-cost[k,:])/cost[k+1]<eps:
-                break   
-                
-        dist=2*(self.CoM.delta)*torch.sqrt((a.sum()+b.sum()-2*self._calcF(P,Q,Omega)))
-        return dist
